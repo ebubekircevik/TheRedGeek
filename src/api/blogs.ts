6de7adf9ad8ebcd5c34eaedffb005eca62c4ffae
ebubekircevik <ebubekircevik666@gmail.com>
2025-07-05 20:35:48 +0300

    mongo connection removed

--- src/api/blogs.ts
+++
@@ -1,8 +0,0 @@
-import { Blog } from "@/types/blog";
-import { httpClient} from "@/api/_core/http";
-
-export const getBlogs = () => httpClient.get<Blog[]>("/blogs");
-export const createBlog = (blog: Omit<Blog, "_id">) => httpClient.post("/blogs", blog);
-export const updateBlog = (id: string, blog: Partial<Blog>) =>
-  httpClient.put(`/blogs/${id}`, blog);
-export const deleteBlog = (id: string) => httpClient.delete(`/blogs/${id}`);
--- src/api/users.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "@/types/user";
-import { httpClient } from "@/api/_core/http";
-
-export const getUsers = () => httpClient.get<User[]>("/users");
-export const createUser = (user: Omit<User, "_id">) => httpClient.post("/users", user);
-export const updateUser = (id: string, user: Partial<User>) =>
-  httpClient.put(`/users/${id}`, user);
-export const deleteUser = (id: string) => httpClient.delete(`/users/${id}`);
